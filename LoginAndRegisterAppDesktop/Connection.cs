b771d6b1f195e9457de54e7fd885b1dd252134c7
Dev205 <rodyael.205@gmail.com>
2023-05-26 11:00:39 +0300

    Connexion au DB

---
+++ LoginAndRegisterAppDesktop/Connection.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace LoginAndRegisterAppDesktop
+{
+    class Connection
+    {
+        private MySqlConnection connect;
+        string serveur;
+        string user;
+        string password;
+        string dataBase;
+        public Connection()
+        {
+            InitializeConnect();
+        }
+
+        /// <summary>
+        /// Initialisation de connection; Methode appeler au constructeur
+        /// </summary>
+        private void InitializeConnect()
+        {
+            serveur = "localhost";
+            dataBase = "etudiant";
+            user = "root";
+            password = "";
+            string connectionString;
+            connectionString = "Data Source=" + serveur + ";Database=" + dataBase + ";User Id=" + user + ";Password=" + password + ";SSL Mode=0";
+            connect = new MySqlConnection(connectionString);
+        }
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                connect.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Impossible de connecter au serveur");
+                        break;
+                    case 1045:
+                        MessageBox.Show("Mot de passe/Username invalid, Try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        public void CloseConnection()
+        {
+            this.connect.Close();
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return this.connect;
+        }
+
+    }
+}
---
+++ LoginAndRegisterAppDesktop/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace LoginAndRegisterAppDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            //this.Close();
+
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
